ee63dec112eb88d3395c34cb47ead89ca5ebb473
byumisato <byumisato@gmail.com>
2023-02-17 17:53:24 -0300

    feat(header): navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+  const [Toggle, showMenu] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
@@ -10,53 +10,53 @@ const Header = () => {
           Sato
         </a>
 
-        <Grid container spacing={0}>
-          <div className="nav__menu">
-            <ul className="nav__list grid">
-              <li className="nav__item">
-                <a href="#home" className="nav__link active-link">
-                  <i className="uil uil-estate nav__icon"></i> Home
-                </a>
-              </li>
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
 
-              <li className="nav__item">
-                <a href="#about" className="nav__link">
-                  <i className="uil uil-user nav__icon"></i> About
-                </a>
-              </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
 
-              <li className="nav__item">
-                <a href="#skills" className="nav__link">
-                  <i className="uil uil-file nav__icon"></i> Skills
-                </a>
-              </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
 
-              <li className="nav__item">
-                <a href="#services" className="nav__link">
-                  <i className="uil uil-briefcase-alt nav__icon"></i> Services
-                </a>
-              </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
 
-              <li className="nav__item">
-                <a href="#portifolio" className="nav__link">
-                  <i className="uil uil-scenery nav__icon"></i> Portifolio
-                </a>
-              </li>
+            <li className="nav__item">
+              <a href="#portifolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portifolio
+              </a>
+            </li>
 
-              <li className="nav__item">
-                <a href="#contact" className="nav__link">
-                  <i className="uil uil-message nav__icon"></i> Contact
-                </a>
-              </li>
-            </ul>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
 
-            <i class="uil uil-times nav__close"></i>
-          </div>
-
-          <div className="nav__toggle">
-            <i class="uil uil-apps"></i>
-          </div>
-        </Grid>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
       </nav>
     </header>
   );
